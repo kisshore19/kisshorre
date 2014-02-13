d781b9d36cced86bc0f45e2cb636ce2933641818
voggu <voggu@blackberry.com>
2014-02-13 16:24:37 +0530

    second commit

--- Struts2Tags/src/main/java/com/struts/actions/DefaultAction.java
+++ Struts2Tags/src/main/java/com/struts/actions/DefaultAction.java
@@ -3,7 +3,11 @@ package com.struts.actions;
 public class DefaultAction{
 	public String execute() throws Exception{
 		System.out.println("action class called");
+		System.out.println("action class called twice");
 		return "success";
+		
+		
+	
 	}
 
 }
